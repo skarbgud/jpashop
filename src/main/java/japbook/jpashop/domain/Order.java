1d3eb14d8142b91030d7a12518f3d285f6906889
GyuHyeong Nam <sjarbgud@gmail.com>
2022-03-06 23:42:02 +0900

    연관관계 편의 메서드(양방향 관계일때 사용)

--- src/main/java/japbook/jpashop/domain/Order.java
+++ src/main/java/japbook/jpashop/domain/Order.java
@@ -36,6 +36,17 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
 
+    //==연관관계 메서드==//
+
+    /* Example
+    Member member = new Member();
+    Order order = new Order();
+
+    member.getOrders().add(order);
+    order.getMember(member);
+    위와 같은 코드를 원자적으로 묶는 메서드를 아래와 같이 만들어 준다.
+    */
+
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
