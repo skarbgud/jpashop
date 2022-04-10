2d4a3219382aa91a47a4da75ae71dbedcf77937e
GyuHyeong Nam <sjarbgud@gmail.com>
2022-04-11 04:10:28 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/japbook/jpashop/domain/OrderItem.java
+++ src/main/java/japbook/jpashop/domain/OrderItem.java
@@ -24,4 +24,30 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    private static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        // OrderItem을 생성한 수량만큼 재고 감소
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        // 주문취소된 만큼 재고 수량을 늘려줘야 한다.
+        getItem().addStock(count);
+    }
+
+    /**
+     *  주문 상품 전체 가격 조회
+     */
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
