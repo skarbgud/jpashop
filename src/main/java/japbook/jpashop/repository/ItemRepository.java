f12928caaa87aa48fcee74f8528da924ae4a275a
GyuHyeong Nam <sjarbgud@gmail.com>
2022-04-10 15:47:29 +0900

    상품 리포지토리 개발

---
+++ src/main/java/japbook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package japbook.jpashop.repository;
+
+import japbook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            // merge는 업데이트랑 비슷한것
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
